004d2d1c2b4fb4ab6f64cecaaab8e19705155638
Chuanhao Qu <chuanhaoq@vmware.com>
2022-10-09 20:55:22 +0800

    add call v2/v3

--- go.mod
+++ go.mod
@@ -1,7 +1,13 @@
-module gitlab.eng.vmware.com/chuanhaoq/naming-demo
+module github.com/go-go-go-go-go/naming-demo
 
 go 1.19
 
-// replace gitlab.eng.vmware.com/chuanhaoq/baby-naming/naming => ../baby-naming
+// replace github.com/go-go-go-go-go/baby-naming => ../baby-naming
 
-// require gitlab.eng.vmware.com/chuanhaoq/baby-naming/naming v0.0.0-00010101000000-000000000000
+// require github.com/go-go-go-go-go/baby-naming v0.0.0-00010101000000-000000000000
+
+require github.com/go-go-go-go-go/baby-naming v1.0.1
+
+require github.com/go-go-go-go-go/baby-naming/v2 v2.0.1
+
+require github.com/go-go-go-go-go/baby-naming/v3 v3.0.1
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 
-	"gitlab.eng.vmware.com/chuanhaoq/baby-naming/naming"
+	naming_v1 "github.com/go-go-go-go-go/baby-naming/naming"
+	naming_v2 "github.com/go-go-go-go-go/baby-naming/v2/naming"
+	naming_v3 "github.com/go-go-go-go-go/baby-naming/v3/naming"
 )
 
 func main() {
-	name := naming.CreateBabyName()
-	fmt.Println(name)
+	name := naming_v1.CreateBabyName()
+	fmt.Println("v1:", name)
+
+	name = naming_v2.CreateBabyName(true)
+	fmt.Println("v2 (male true):", name)
+	name = naming_v2.CreateBabyName(false)
+	fmt.Println("v2 (male false):", name)
+
+	name = naming_v3.CreateBabyName(true, 5)
+	fmt.Println("v3 (male true):", name)
+	name = naming_v3.CreateBabyName(false, 5)
+	fmt.Println("v3 (male false):", name)
 }
